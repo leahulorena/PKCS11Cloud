761383a62d220a487c496795203a9005e6e5922e
leahulorena <lorena.leahu@gmail.com>
2018-06-10 14:14:11 +0300

    commit softhsm pkcs11 driver

---
+++ softhsm/dllmain.cc
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <Windows.h>
+
+#pragma unmanaged
+
+__declspec(dllexport) BOOL WINAPI
+DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpvReserved)
+{
+    hModule = hModule;
+    lpvReserved = lpvReserved;
+	
+    switch (ul_reason_for_call) { 
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH: 
+    case DLL_PROCESS_DETACH: 
+    default:
+        break;
+    }
+    return (TRUE);
+}
+
+#pragma managed
---
+++ wrapper/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+namespace wrapper
+{
+    public class Class1
+    {
+
+
+        
+
+        [DllExport]
+        static public string test() {
+            return "test";
+
+        }
+        
+        [DllExport]
+        static public string init()
+        {
+            localhost.DSWS ws = new localhost.DSWS();
+           // String str = Marshal.PtrToStringAnsi(getUsername());
+            string str = "client1@client.com";
+            string file = ws.GetUserCertificate(str);
+            return file;
+        }
+
+        //[DllImport("softhsm.dll", CallingConvention = CallingConvention.Cdecl)]
+        //static extern IntPtr getUsername();
+
+        //[DllImport("softhsm.dll", CallingConvention = CallingConvention.Cdecl)]
+        //static extern IntPtr setUsername([MarshalAs(UnmanagedType.LPTStr)]string issuer);
+    }
+}
